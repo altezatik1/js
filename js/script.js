f489d58c8a52110fdca5695605946bc96e0efd05
Artem PP <alte@bk.ru>
2019-11-23 11:34:44 +1000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let tempCelsiul = 32;
+alert ('Температура по цельсию 32 градуса');
+let tempFar = (9 / 5) * tempCelsiul + 32;
+alert ('По фаренгейту  ' + tempFar);
+
+var admin = '';
+var name = 'Василий';
+admin = name;
+alert (admin + " теперь админ");
+
+var a = 20;
+var b = 30;
+a = a + b; // 50
+b = a - b; // 20
+a = a - b;
+alert ('a= ' + a + ' b= ' + b);
+
+
+var s = 1000 + "108";
+
+alert ('1000 +"108" =' + s );
