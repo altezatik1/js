9f8feecbfc03704ac64914269a5072767608df8e
Artem PP <alte@bk.ru>
2019-12-07 11:14:52 +1000

    23

--- js/script.js
+++ js/script.js
@@ -1,45 +1,62 @@
-//1 
+function createNewObject(num) {
+  num = +prompt ('введите число');
+     let pull = {
+      hundred:null,
+      dozen:null,
+      units:null
+     }
+     
+      if (num <= 999) {
+      let str = num + '';
+      pull.units = str[2];
+      pull.dozen = str[1];
+      pull.hundred = str[0];
+        console.log(pull);
+      }
+      else {
+        console.log(pull);
+      }
+}
+
+createNewObject()
 
 
-function simpleNumber (num) {
-  if( num < 2 ) return false;
-  let count = 2;
-  while(count < num) {
-    if(num % count === 0) {
-      return false;
+
+
+//база
+let PRODUCTS_NAMES = ['Кофе','Сахар','Сливки','Соль','Перец','Паприка'];
+let PRICES = [150,240,110,230,121,413];
+let IDS = [0,1,2,3,4,5];
+
+
+let products = [];
+
+let cart = {
+  items: [],
+  total: 0
+}
+
+let data = {
+    items: [],
+    getData: function () {
+      for (i = 0; i < IDS.length;i++){
+       products.push (createNewProduct(i))
     }
-    count++;
   }
-  return true;
 }
 
-function get100(max) {
-  let i = 0;
-  let list = [];
-
-  while (i < max) {
-    if(simpleNumber(i)) list.push(i);
-    i++;
+function createNewProduct (index) {
+  return {
+    product_name: PRODUCTS_NAMES [index],
+    price: PRICES [index],
+    product_id: IDS [index]
   }
-  console.log(list);
 }
 
-get100(100);
-
-
-
-
-
-//4
-
-for (var i = 0; i < 9; i++, console.log(i)) {}
-
-
-//5
-
-var cc = "";
-for(var i = 1; i < 20; i++) {
- cc = cc +"#";
-console.log(cc)
+function addProduct (id) {
+  let find = products.find (el => el.product_id === id)
+  cart.items.push (Object.assign ({},find,{quantity: 1}))
 }
+
+
 
